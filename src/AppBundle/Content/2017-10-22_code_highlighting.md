6d763566bde9ec91fea534586cc3167373baa1d6
Todd Eidson <todd@eidson.info>
2017-10-24 07:46:25 -0400

    Started

---
+++ src/AppBundle/Content/2017-10-22_code_highlighting.md
@@ -0,0 +1,16 @@
+# Code Highlighting in HTML
+
+I've been wanting to start blogging for quite some time now, and I figured there is no time like the present.
+I am familiar with both wordpress and Drupal, but I wanted to "roll my own" both for flexibility and as a learning experience.
+Since my blog was more than likely going to be code heavy, one thing that was really important to me was code highlighting in the display.
+
+```html
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
+```
+
+```html
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+```
+
+
+[highlight.js](https://highlightjs.org/)
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use League\CommonMark\CommonMarkConverter;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $basedir = realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR;
+
+        $posts = [];
+        $files = array_reverse(scandir($basedir . 'src/AppBundle/Content'));
+        foreach ($files as $file) {
+            $text = file_get_contents($basedir . 'src/AppBundle/Content/' . $file);
+            $posts[] = [
+                'html' => $this->mdToHtml($text),
+                'uri' => $this->generateUrl(
+                    'post',
+                    ['post' => str_replace('.md', '', $file)]
+                )
+                ];
+        }
+
+        return $this->render('default/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/post/{post}", name="post")
+     */
+    public function postAction($post)
+    {
+        $basedir = realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR;
+        $text = file_get_contents($basedir . "src/AppBundle/Content/$post.md");
+
+        $html = $this->mdToHtml($text);
+        return $this->render('default/post.html.twig', [
+            'post' => $post,
+            'html' => $html
+        ]);
+    }
+
+    /**
+     * @Route("/resume", name="resume")
+     */
+    public function resumeAction()
+    {
+        return $this->render(':default:resume.html.twig');
+    }
+
+    /**
+     * @Route("/aboutMe", name="aboutMe")
+     */
+    public function aboutMeAction()
+    {
+        return $this->render(':default:aboutMe.html.twig');
+    }
+
+
+    /**
+     * @param $text
+     * @return string
+     */
+    public function mdToHtml($text)
+    {
+        $converter = new CommonMarkConverter();
+        $parser = new \cebe\markdown\GithubMarkdown();
+        $parser->html5 = true;
+        $html = $converter->convertToHtml($text);
+        $html = $parser->parse($text);
+        return $html;
+    }
+
+}
