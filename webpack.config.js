ed625ba19a17d723a783b578cca2ed4703628624
Todd Eidson <todd@eidson.info>
2017-10-27 15:29:41 -0400

    Use webpack for scss - screen size image widths

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+
+    .addStyleEntry('global', './src/AppBundle/Resources/scss/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+// create hashed filenames (e.g. app.abc123.css)
+// .enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
