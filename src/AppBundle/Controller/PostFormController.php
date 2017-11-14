3a040591d5847e1ad2b2b405f834c71f2b63dd85
Todd Eidson <todd@eidson.info>
2017-11-13 20:15:30 -0500

    Handle form submission - persist to database

--- src/AppBundle/Controller/PostFormController.php
+++ src/AppBundle/Controller/PostFormController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Post;
 use AppBundle\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostFormController extends Controller
 {
@@ -13,10 +14,18 @@ class PostFormController extends Controller
      * @Route("/admin/form/post", name="")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+              $em = $this->getDoctrine()->getManager();
+              $em->persist($post);
+              $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
         return $this->render(
             ':default:post.form.html.twig',
             ['form' => $form->createView()]
