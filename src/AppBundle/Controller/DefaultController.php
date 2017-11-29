2372b7765fea9348a5a37d1778332efad9756bed
Todd Eidson <todd@eidson.info>
2017-11-28 20:34:22 -0500

    Remove aboutMeAction - code cleanup

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,52 +4,51 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      * @Route("/posts")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $posts = $this->getDoctrine()
             ->getRepository('AppBundle:Post')
             ->findBy([], ['published' => 'DESC']);
 
         return $this->render('default/index.html.twig', [
-            'posts' => $posts
+            'posts' => $posts,
         ]);
     }
 
     /**
      * @Route("/post/{post}", name="post")
+     *
+     * @param $post
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function postAction($post)
     {
-        $post =  $this->getDoctrine()
+        $post = $this->getDoctrine()
             ->getRepository('AppBundle:Post')
             ->findOneBy(['url' => $post]);
 
         return $this->render('default/post.html.twig', [
-            'post' => $post
+            'post' => $post,
         ]);
     }
 
     /**
      * @Route("/resume", name="resume")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function resumeAction()
     {
         return $this->render(':default:resume.html.twig');
-    }
-
-    /**
-     * @Route("/aboutMe", name="aboutMe")
-     */
-    public function aboutMeAction()
-    {
-        return $this->render(':default:aboutMe.html.twig');
     }
 }
