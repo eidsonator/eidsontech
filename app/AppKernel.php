aaf9c1b65d81b690fe75349ca4007eacf6ca1f51
Todd Eidson <todd@eidson.info>
2017-12-02 06:03:10 -0500

    Add log viewer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new EmanueleMinotto\SimpleArrayBundle\SimpleArrayBundle(),
+            new Greenskies\WebLogViewerBundle\WebLogViewerBundle(),
             new AppBundle\AppBundle(),
         ];
 
